4fde31d0a0a6b5f46aa27cb61a6afea38dd7d5fd
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-12-02 17:08:35 +0000

    Day one complete
    Doing some benchmarking to compare Hash with set

---
+++ day_1_chronal_calibration/frequency_processor.rb
@@ -0,0 +1,37 @@
+frequencies = File.readlines('input.txt').map(&:to_i)
+
+# sum of input
+p frequencies.reduce(0, &:+)
+
+# first frequency reached twice
+require 'set'
+require 'benchmark'
+
+# Set feels more idiomatic but is slower
+using_set = Benchmark.measure do
+  current_total = 0
+
+  reached_once = Set.new
+  frequencies.cycle do |freq|
+    current_total += freq
+    break p  "frequency twice: #{current_total}" if reached_once.include?(current_total)
+    reached_once.add(current_total)
+  end
+end
+p using_set
+
+
+# Using Hash is more performant, however it feels weird making a key value pair
+# for performance optimisation only
+using_hash = Benchmark.measure do
+  current_total = 0
+  reached_once = {}
+
+  frequencies.cycle do |freq|
+    current_total += freq
+    break p "frequency twice: #{current_total}" if reached_once[current_total]
+    reached_once[current_total] = current_total
+  end
+end
+
+p using_hash
